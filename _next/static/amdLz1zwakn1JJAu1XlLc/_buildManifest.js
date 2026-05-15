d2b08e9bff9e32887f14842a6e4a6c2e0fbfa4e4
jalfredobaltomora-jpg <jalfredobaltomora-jpg@users.noreply.github.com>
2026-05-15 18:00:10 +0000

    deploy: eff9dec43b4de4a55cc316bd059d103fc67fd2d9

--- _next/static/amdLz1zwakn1JJAu1XlLc/_buildManifest.js
+++ _next/static/amdLz1zwakn1JJAu1XlLc/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/v0-ailoginandpanel//_next/:path+",
-        "destination": "/v0-ailoginandpanel/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
